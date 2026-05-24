165d95d6c7bbb1f44a6e0e3e2ca81e67eaf6849a
idontknowbutimhere <arthurpigeon27@gmail.com>
2026-05-24 13:55:50 -0400

    Add swimmers data to data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+const swimmers = [
+  {
+    id: "SWCAN001",
+    name: "Arthur Pigeon",
+    year: 2011,
+    club: "RL"
+  },
+
+  {
+    id: "SWCAN002",
+    name: "John Doe",
+    year: 2010,
+    club: "Sharks"
+  }
+];
